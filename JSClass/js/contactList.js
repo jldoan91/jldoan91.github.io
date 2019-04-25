1b18002301149ad5a738b323c13e907017662aac
Josh Doan <joshd@TagPay.com>
2019-04-25 11:44:30 -0700

    added nickname field to expand search function

--- JSClass/js/contactList.js
+++ JSClass/js/contactList.js
@@ -1,5 +1,6 @@
 //initialize input fields and display divs
 const contactName = $("#contactName");
+const contactNN = $("#contactNickname");
 const contactPhone = $("#contactPhone");
 const contactEmail = $("#contactEmail");
 const search = $("#searchVal");
@@ -8,9 +9,10 @@ const contactDisplay = $("#contactList");
 var contacts;
 var contactList;
 
-const storeList = function(val){
-  localStorage.setItem("contacts", JSON.stringify(val));
-}
+//function to store list of contacts in localstorage
+const storeList = function(list) {
+  localStorage.setItem("contacts", JSON.stringify(list));
+};
 
 //function to check localstorage for existing contact list
 const listCheck = function() {
@@ -32,11 +34,7 @@ const displayList = function() {
     //loop through contact list and display each contact
     contacts.forEach(function(val) {
       contactDisplay.append(
-        `<li> Name: <span class="listName">${
-          val.name
-        }</span><br><span class="delete far fa-trash-alt"></span> Phone: ${
-          val.phone
-        }<br>Email: ${val.email}`
+        `<li> Name: <span class="listName">${val.name}</span><br><span class="delete far fa-trash-alt"></span> Phone: ${val.phone}<br>Email: ${val.email}`
       );
     });
   }
@@ -53,7 +51,7 @@ $("#addContactBtn").on("click", function() {
     contactPhone.val().length === 0 ||
     contactEmail.val().length === 0
   ) {
-    alert("all three fields are required!");
+    alert("Name, Phone number, and Email are required!");
     //verify there's at least 10 digits in the phone number and no letters
   } else if (
     contactPhone.val().replace(/[^0-9]/gi, "").length < 10 ||
@@ -65,25 +63,21 @@ $("#addContactBtn").on("click", function() {
     var contact = {};
     //build single contact from input entries
     contact.name = contactName.val();
+    contact.nickname = contactNN.val();
     contact.phone = contactPhone.val();
     contact.email = contactEmail.val();
     //push contact to array of contacts
     contactList.push(contact);
     contactName.val("");
+    contactNN.val("");
     contactPhone.val("");
     contactEmail.val("");
     contactDisplay.append(
-      `<li>Name: <span class="listName">${
-        contact.name
-      }</span><br><span class="delete far fa-trash-alt"></span> Phone: ${
-        contact.phone
-      }<br>Email: ${contact.email}`
+      `<li>Name: <span class="listName">${contact.name}</span><br><span class="delete far fa-trash-alt"></span> Phone: ${contact.phone}<br>Email: ${contact.email}`
     );
   }
   //add contact object to contact list array
   storeList(contactList);
-  //call displayList function to refresh contact display
-  // displayList();
 });
 
 //add event listener search button
@@ -98,18 +92,9 @@ $("#searchBtn").on("click", function() {
     //loop through the list of contacts in localstorage
     contacts.forEach(function(contact) {
       //check if the searched name, email, or phone number matches a stored contact
-      if (
-        contact.name.toLowerCase().includes(search.val().toLowerCase()) ||
-        contact.email.includes(search.val()) ||
-        search.val().replace(/[^0-9]/gi, "") ===
-          contact.phone.replace(/[^0-9]/gi, "")
-      ) {
+      if (contact.name.toLowerCase().includes(search.val().toLowerCase()) || contact.nickname.toLowerCase().includes(search.val().toLowerCase()) || contact.email.includes(search.val()) || search.val().replace(/[^0-9]/gi, "") === contact.phone.replace(/[^0-9]/gi, "")) {
         searchDisplay.append(
-          `<li>Name: <span class="listName">${
-            contact.name
-          }</span><br><span class="delete far fa-trash-alt"></span> Phone: ${
-            contact.phone
-          }<br> Email: ${contact.email}`
+          `<li>Name: <span class="listName">${contact.name}</span><br><span class="delete far fa-trash-alt"></span> Phone: ${contact.phone}<br> Email: ${contact.email}`
         );
         //check if the search value matches a contacts phone number without the leading digit
       } else if (
@@ -117,11 +102,7 @@ $("#searchBtn").on("click", function() {
         contact.phone.replace(/[^0-9]/gi, "").substring(1)
       ) {
         searchDisplay.append(
-          `<li>Name: <span class="listName">${
-            contact.name
-          }</span><br><span class="delete far fa-trash-alt"></span> Phone: ${
-            contact.phone
-          }<br> Email: ${contact.email}`
+          `<li>Name: <span class="listName">${contact.name}</span><br><span class="delete far fa-trash-alt"></span> Phone: ${contact.phone}<br> Email: ${contact.email}`
         );
       }
     });
@@ -145,7 +126,7 @@ $("ul").on("click", ".delete", function(event) {
   });
   //set localstorage to new array without the clicked contact
   storeList(contactList);
-  $(this).parent().fadeOut(500, function(){
-    $(this).remove();
-  });
+  $(this).parent().fadeOut(500, function() {
+      $(this).remove();
+    });
 });
